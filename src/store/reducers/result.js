13bede887c4c8a49530f125b9a62ed8828a36531
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-08-19 17:36:28 -0500

    add a leaner switch case statment

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
 	results: []
 };
 
+const deleteResult = (state, action) => {
+	const updatedArray = state.results.filter((result) => result.id !== action.resultElId);
+	return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.INCREMENT:
@@ -16,13 +21,7 @@ const reducer = (state = initialState, action) => {
 			return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
 
 		case actionTypes.DELETE_RESULT:
-			// const id = 2;
-			// const newArray = [ ...state.results ];
-			// newArray = newArray.splice(id, 1);
-
-			const updatedArray = state.results.filter((result) => result.id !== action.resultElId);
-
-			return updateObject(state, { results: updatedArray });
+			return deleteResult(state, action);
 	}
 
 	return state;
